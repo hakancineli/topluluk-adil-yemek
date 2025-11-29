dec3116a73b65ad37f76142db0a363a9146a888d
Hakan Cineli <hakancineli@example.com>
2025-11-30 02:50:01 +0300

    Add: Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
 import AdminProtectedRoute from './components/AdminProtectedRoute'
@@ -97,6 +98,7 @@ function App() {
           </main>
         </div>
       </NotificationProvider>
+      <Analytics />
     </Router>
   )
 }
